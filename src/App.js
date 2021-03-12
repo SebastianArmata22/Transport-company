b472847b5b1fe899c64b86978d07c69b41e97541
unknown <arkadiuszhorwat@gmail.com>
2021-03-12 11:20:59 +0100

    changed accepted

--- src/App.js
+++ src/App.js
@@ -3,11 +3,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
       <h1>Horwat Arkadiusz</h1>
-=======
       <h1>Bartosz Bednarczyk</h1>
->>>>>>> a9892958e9b035172e5a212f5d5a674bf933c5d0
     </div>
   );
 }
