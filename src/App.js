f6d2a73f0f01f6c0be06317e0bcd07eab39e4473
Bartosz <bfbednarczyk@gmail.com>
2021-03-12 11:24:46 +0100

    Added Navbar

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import Navbar from './components/Navbar/Navbar'
 
 function App() {
   return (
     <div className="App">
+      <Navbar />
       <h1>Horwat Arkadiusz</h1>
       <h1>Bartosz Bednarczyk</h1>
     </div>
