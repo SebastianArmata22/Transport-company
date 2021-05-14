bcad74cc7969af2387042bd10e782eaa5c21ecca
unknown <arkadiuszhorwat@gmail.com>
2021-05-14 10:17:03 +0200

    remove h1 tags

--- src/App.js
+++ src/App.js
@@ -3,11 +3,6 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
-=======
-      <h1>Horwat Arkadiusz</h1>
->>>>>>> b472847b5b1fe899c64b86978d07c69b41e97541
-      <h1>Bartosz Bednarczyk</h1>
     </div>
   );
 }
