c4c1da6f32d7b27fc6ea653b3816f9a862ecabd4
unknown <arkadiuszhorwat@gmail.com>
2021-06-23 11:20:05 +0200

    update app.js

--- src/App.js
+++ src/App.js
@@ -6,28 +6,18 @@ import Homepage from './components/Homepage/Homepage';
 import Contactpage from './components/Contactpage/Contactpage'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import AccountPage from './components/account/AccountPage';
-<<<<<<< HEAD
-import CheckingPage from './components/checkingPage/CheckingPage';
-
-=======
 import AdminPage from './components/admins/AdminPage';
->>>>>>> a529c8e655b14e73ad0840590837cd8423f730aa
 
 function App() {
   return (
     <div className="App">
        <Router>
             <Switch>
-<<<<<<< HEAD
-            <Route path="/checking">
-                <CheckingPage />
-=======
               <Route path="/" exact>
                 <Homepage />
               </Route>
               <Route path="/contact">
                 <Contactpage />
->>>>>>> a529c8e655b14e73ad0840590837cd8423f730aa
               </Route>
               <Route path="/login">
                 <Login />
